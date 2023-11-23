8ca0f8f7b1a113f8453a6bd964de54f690f1744f
UnrealN1K <nashwaan.khan@gmail.com>
2023-11-22 18:03:21 -0800

    Added editing and retrieval of info with sample queries

--- Database/Database.sql
+++ Database/Database.sql
@@ -1,45 +1,46 @@
 -- Create Users table
 CREATE TABLE users (
     user_id SERIAL PRIMARY KEY,
-    username VARCHAR(50) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
-    preferences VARCHAR(255) -- You may want to use JSON or another format for preferences
+    email VARCHAR(255) NOT NULL, -- Added email field
 );
 
 -- Create Movies table
 CREATE TABLE movies (
+    movie_id SERIAL PRIMARY KEY, -- Added movie_id as a primary key
     title VARCHAR(255) NOT NULL,
     summary TEXT,
     image_url VARCHAR(255),
+    likes INTEGER DEFAULT 0, -- Added likes field with a default value of 0
+    dislikes INTEGER DEFAULT 0, -- Added dislikes field with a default value of 0
+    genres VARCHAR(255)[] -- Changed genres to an array type
 );
 
--- Sample Query 1: Retrieve all users
-SELECT * FROM users;
+-- Sample Query 1: Add a new user with email
+INSERT INTO users (password, email)
+VALUES ('password', 'nashwaan@veldt.co');
 
--- Sample Query 2: Retrieve all movies
-SELECT * FROM movies;
+-- Sample Query 2: Add a new movie with likes, dislikes, and genres
+INSERT INTO movies (title, summary, image_url, likes, dislikes, genres)
+VALUES ('titanic', 'boring', 'http://example.com/image.jpg', 10, 2, ARRAY['Romance', 'Adventure']);
 
--- Sample Query 3: Add a new user
-INSERT INTO users (username, password, preferences) VALUES ('john_doe', 'password123', '{"genre": "Drama", "language": "English"}');
+-- Sample Query 3: Retrieve users and their emails
+SELECT user_id, email FROM users;
 
--- Sample Query 4: Add a new movie
-INSERT INTO movies (title, summary, image_url) VALUES ('Sample Movie', 'A great movie', 'http://example.com/image.jpg');
+-- Sample Query 4: Retrieve movies with high likes
+SELECT title, likes FROM movies WHERE likes > 5;
 
--- Sample Query 5: Retrieve users and their preferences
-SELECT username, preferences FROM users;
+-- Sample Query 5: Update user email
+UPDATE users SET email = 'nkhan040@ucr.edu' WHERE user_id = 1;
 
--- Sample Query 6: Retrieve movies with a specific genre
-SELECT title, summary FROM movies WHERE 'Action' = ANY(string_to_array(genres, ', '));
+-- Sample Query 6: Update movie likes
+UPDATE movies SET likes = likes + 1 WHERE title = 'titanic';
 
--- Sample Query 7: Update user preferences
-UPDATE users SET preferences = '{"genre": "Comedy", "language": "Spanish"}' WHERE username = 'john_doe';
+-- Sample Query 7: Delete a movie by title
+DELETE FROM movies WHERE title = 'titanic';
 
--- Sample Query 8: Delete a movie
-DELETE FROM movies WHERE title = 'Sample Movie';
-
--- Sample Query 9: Count the number of users
+-- Sample Query 8: Count the number of users
 SELECT COUNT(*) FROM users;
 
--- Sample Query 10: Count the number of movies
+-- Sample Query 9: Count the number of movies
 SELECT COUNT(*) FROM movies;
-
