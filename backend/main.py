ccc1710c620ab4ec01c89a9851f4a5fe33e7247a
UnrealN1K <nashwaan.khan@gmail.com>
2023-12-08 00:52:25 -0800

    Added variables for environment

--- backend/main.py
+++ backend/main.py
@@ -11,3 +11,14 @@ from typing import List
 
 from sqlalchemy import create_engine
 
+# Load environment variables from .env file
+load_dotenv()
+
+# Retrieve variables from environment
+db_user = os.getenv('DB_USER')
+db_password = os.getenv('DB_PASSWORD')
+db_host = os.getenv('DB_HOST')
+db_port = os.getenv('DB_PORT')
+db_name = os.getenv('DB_NAME')
+
+
