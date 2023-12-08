b63903041797b0f85a93b08659287d77d56ec98e
Bao-Huynh888 <bhuyn048@ucr.edu>
2023-12-08 01:19:22 -0800

    Queries from the table and sessions

--- backend/sql-alchemy.py
+++ backend/sql-alchemy.py
@@ -81,3 +81,33 @@ session = Session()
 # u = User("hollandpleskac@gmail.com")
 # session.add(u) # add person to db
 # session.commit() # apply changes to db
+
+# # Add row to table
+# m = Movie("Movie2", "desc", "https://www.twincities.com/wp-content/uploads/2022/05/Summer_Film_Preview_71939.jpg")
+# session.add(m)
+# session.commit()
+
+# # Add row to table
+# m = Rating(u.email, m.id, 4)
+# session.add(m)
+# session.commit()
+
+
+# Query from table
+results = session.query(User).all() # select everything from people table
+results_filtered = session.query(User).filter(User.email == "hples001@gmail.com") # filter
+# for r in results_filtered:
+#     print(r)
+
+# results = session.query(Rating).all() # select everything from people table
+# print(results)
+
+# res = session.query(User).filter(User.email == "hples001@gmail.com").all()
+# print(res)
+
+results = session.query(Rating).join(User, "hples001@gmail.com" == Rating.userEmail).join(Movie, Movie.id == Rating.movieId).all()
+print(results)
+
+# Query from table (things owned by person)
+# results = session.query(Thing, Person).filter(Thing.owner == Person.ssn).filter(Person.firstname == "holland").all()
+# print("res",results)```
