62e4b270cbe1d681f0b32bb91c7c0f1195db7c27
UnrealN1K <nashwaan.khan@gmail.com>
2023-12-08 01:13:22 -0800

    Added database variables for environment

--- backend/sql-alchemy.py
+++ backend/sql-alchemy.py
@@ -3,3 +3,13 @@ from dotenv import load_dotenv
 from sqlalchemy import create_engine, ForeignKey, Column, String, Integer, CHAR
 from sqlalchemy.orm import declarative_base, sessionmaker
 
+# Load environment variables from .env file
+load_dotenv()
+
+# Retrieve variables from environment
+db_user = os.getenv('DB_USER')
+db_password = os.getenv('DB_PASSWORD')
+db_host = os.getenv('DB_HOST')
+db_port = os.getenv('DB_PORT')
+db_name = os.getenv('DB_NAME')
+
